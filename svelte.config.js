8542eaa4ec7bbc5de3ac6e4ad0975e9a3ae93870
Saveliy <miheev.saveliy.2002@gmail.com>
2024-05-26 12:20:36 +0800

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
+import adapter from '@sveltejs/adapter-static'
+/** @type {import('@sveltejs/kit').Config} */
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base:
+				process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
+		}
+	}
 }
